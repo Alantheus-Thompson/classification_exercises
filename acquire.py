18d7da8aad4f1f27d742caa097280b70a9f64ba2
Alantheus Thompson <Alantheus.Thompson@gmail.com>
2023-11-21 14:40:09 -0600

    complete

--- acquire.py
+++ acquire.py
@@ -1,33 +1,52 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "ef46e7de",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.11.5"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+from env import get_db_url
+import pandas as pd
+from pydataset import data
+import os
+
+def get_titanic_data():
+    filename = "titanic.csv"
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename, index_col=0)
+    else:
+        url = get_db_url('titanic_db')
+        df_titanic = pd.read_sql(('SELECT * FROM passengers'), url)
+        df_titanic.to_csv(filename)
+    return df_titanic
+
+def get_iris_data():
+    filename = "iris.csv"
+    
+    if os.path.isfile(filename):
+        return pd.read_csv(filename, index_col=0)
+    
+    else:
+        url=get_db_url('iris_db')
+        df_iris = pd.read_sql(('''
+        select *
+        from measurements
+        join species
+            using (species_id)'''),url)
+        df_iris.to_csv(filename)
+    return df_iris
+
+def get_telco_data():
+    filename = "churn.csv"
+        
+    if os.path.isfile(filename):
+        return pd.read_csv(filename, index_col=0)
+
+    else:
+        url = get_db_url('telco_churn')
+        df_telco_churn = pd.read_sql(('''
+        SELECT * From customers
+        join contract_types
+            using (contract_type_id)
+        join internet_service_types
+            using(internet_service_type_id)
+        join payment_types
+            using(payment_type_id)'''), url)
+        df_telco_churn.to_csv(filename)
+    return df_telco_churn     
+  
+
